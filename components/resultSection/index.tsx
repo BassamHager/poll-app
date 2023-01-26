fea77bd2771dabbe010914315f7701a065d12f1c
Douaa Hassan <douaa.hasan@new10.com>
2023-01-26 23:35:01 +0100

    updated showing the content of the result section conditionally

--- components/resultSection/index.tsx
+++ components/resultSection/index.tsx
@@ -1,28 +1,25 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import Section from "../section";
 import { AppContext } from "@/context/AppContext";
 import ResultSectionFooter from "./resultSectionFooter";
-import { AnswerType } from "@/types/typings";
 import ResultSectionBody from "./resultSectionBody";
 
 type Props = {};
 
 export default function ResultSection({}: Props) {
-  const { pollQuestion } = useContext(AppContext);
-  // let answers: AnswerType[];
-
-  // useEffect(() => {
-  //   answers = pollAnswers.map((answer: AnswerType) => ({
-  //     value: answer.value,
-  //     totalVotes: 0,
-  //   }));
-  // }, []);
+  const { pollQuestion, pollAnswers } = useContext(AppContext);
 
   return (
-    <Section
-      header={<h2>{pollQuestion}</h2>}
-      body={<ResultSectionBody />}
-      footer={<ResultSectionFooter />}
-    />
+    <>
+      {pollQuestion && pollAnswers.length > 1 ? (
+        <Section
+          header={<h2>{pollQuestion}</h2>}
+          body={<ResultSectionBody />}
+          footer={<ResultSectionFooter />}
+        />
+      ) : (
+        <section></section>
+      )}
+    </>
   );
 }
